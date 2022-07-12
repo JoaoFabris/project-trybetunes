213fd60587d117e71402155febbf70f00db3033b
João Fabris <jvfabris21@gmail.com>
2022-07-11 23:09:19 -0300

    Requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from './Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   constructor() {
     super();
     this.onChangeInput = this.onChangeInput.bind(this);
     this.validadeBtn = this.validadeBtn.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
       searchName: '',
+      artist: '',
       disableBtn: true,
+      array: [],
+      searching: false,
+      loading: false, // searchLoading
+
     };
+  }
+
+  handleSubmit(event) {
+    const { searchName } = this.state;
+    this.setState({ loading: true, artist: searchName, searching: true }, async () => {
+      const array = await searchAlbumsAPI(searchName);
+      this.setState({ loading: false, array, searchName: '' });
+    });
+    event.preventDefault();
   }
 
   onChangeInput({ target }) {
@@ -17,6 +35,40 @@ class Search extends Component {
     this.setState({
       [name]: value,
     }, () => this.validadeBtn());
+  }
+
+  loaded = () => {
+    const { loading } = this.state;
+    return (loading ? <Loading className="search-loading" /> : null);
+  }
+
+  results() {
+    const { array, artist } = this.state;
+    return (
+      array.length === 0 ? <h2>Nenhum álbum foi encontrado</h2>
+        : (
+          <div>
+            <h4>
+              { `Resultado de álbuns de: ${artist}` }
+            </h4>
+            <div>
+              {array.map(
+                (a) => (
+                  <div key={ a.collectionId }>
+                    <img src={ a.artworkUrl100 } alt={ a.collectionName } />
+                    <Link
+                      to={ `/album/${a.collectionId}` }
+                      data-testid={ `link-to-album-${a.collectionId}` }
+                    >
+                      {a.collectionName}
+                    </Link>
+                  </div>
+                ),
+              )}
+            </div>
+          </div>
+        )
+    );
   }
 
   validadeBtn() {
@@ -30,7 +82,11 @@ class Search extends Component {
   }
 
   render() {
-    const { searchName, disableBtn } = this.state;
+    const {
+      searchName,
+      disableBtn,
+      searching,
+    } = this.state;
     return (
       <div>
         <Header />
@@ -47,13 +103,18 @@ class Search extends Component {
             />
           </label>
           <button
-            type="button"
+            type="submit"
             data-testid="search-artist-button"
             disabled={ disableBtn }
+            onClick={ this.handleSubmit }
           >
             Pesquisar
           </button>
         </form>
+        <div>
+          {searching && this.loaded()}
+          {searching && this.results()}
+        </div>
       </div>
     );
   }
