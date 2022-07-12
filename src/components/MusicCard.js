f285c6f0ff6e6fdc210ffa39eb655db7bbde8cb7
João Fabris <jvfabris21@gmail.com>
2022-07-11 23:34:49 -0300

    Requisito 7

---
+++ src/components/MusicCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MusicCard extends React.Component {
+  render() {
+    const { trackName, previewUrl } = this.props;
+    return (
+      <li>
+        <h2>{ trackName }</h2>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          <code>audio</code>
+        </audio>
+      </li>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  previewUrl: PropTypes.string.isRequired,
+  trackName: PropTypes.string.isRequired,
+};
+
+export default MusicCard;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,15 +1,69 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      arraymusic: [],
+      inf: {},
+    };
+  }
+
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const musicsList = await getMusics(id);
+    const inf = musicsList[0];
+    this.setState({ arraymusic: musicsList.slice(1), inf });
+  }
+
   render() {
+    const { arraymusic, inf } = this.state;
+    const { artistName, collectionName } = inf;
     return (
       <>
         <Header />
-        <div data-testid="page-album"> </div>
+        <div data-testid="page-album">
+          <div>
+            <h2 data-testid="artist-name">{artistName}</h2>
+            <h3 data-testid="album-name">{collectionName}</h3>
+            <div>
+              <div>
+                {arraymusic.map((music, i) => (
+                  <div key={ i }>
+                    <audio
+                      data-testid="audio-component"
+                      src={ music.previewUrl }
+                      controls
+                    >
+                      <track kind="captions" />
+                      O seu navegador não suporta o elemento
+                      {' '}
+                      <code>audio</code>
+                      .
+                    </audio>
+                    <span>{music.trackName}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
 }
+
+Album.propTypes = {
+  id: PropTypes.string,
+  match: PropTypes.objectOf(PropTypes.any),
+};
+
+Album.defaultProps = {
+  id: '', match: '',
+};
 
 export default Album;
